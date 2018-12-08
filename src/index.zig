0a8e730c4ff4e4288589cc744a78cdb6826f6e10
dbandstra <dbandstra@protonmail.com>
2018-12-08 14:25:50 -0800

    update index.zig

--- src/index.zig
+++ src/index.zig
@@ -1,8 +1,22 @@
-pub const Seekable = @import("traits/Seekable.zig").Seekable;
+pub const image = @import("image/image.zig");
+pub const ppm = @import("image/ppm.zig");
+pub const raw = @import("image/raw.zig");
+pub const tga = @import("image/tga.zig");
 
-pub const FileInStream = @import("FileInStream.zig").FileInStream;
+pub const StackAllocator = @import("traits/StackAllocator.zig").StackAllocator;
+
+pub const ArrayListOutStream = @import("ArrayListOutStream.zig").ArrayListOutStream;
+pub const DoubleStackAllocator = @import("DoubleStackAllocator.zig").DoubleStackAllocator;
 pub const InflateInStream = @import("InflateInStream.zig").InflateInStream;
+pub const Inflater = @import("Inflater.zig").Inflater;
 pub const LineReader = @import("LineReader.zig").LineReader;
+pub const OwnerId = @import("OwnerId.zig").OwnerId;
 pub const ScanZip = @import("ScanZip.zig").ScanZip;
-pub const SimpleInStream = @import("SimpleInStream.zig").SimpleInStream;
-pub const SimpleOutStream = @import("SimpleOutStream.zig").SimpleOutStream;
+pub const SingleStackAllocator = @import("SingleStackAllocator.zig").SingleStackAllocator;
+const SliceStream = @import("SliceStream.zig");
+pub const SliceWithCursor = SliceStream.SliceWithCursor;
+pub const SliceInStream2 = SliceStream.SliceInStream2;
+pub const SliceSeekableStream = SliceStream.SliceSeekableStream;
+pub const SliceSeekableStreamAlt = @import("SliceStream2.zig").SliceSeekableStreamAlt;
+pub const c = @import("c.zig");
+pub const util = @import("util.zig");
